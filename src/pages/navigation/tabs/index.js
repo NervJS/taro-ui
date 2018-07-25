b10ed1349ae2049689ecb666b5f26e95352d7c3e
jimczj <jimczj@gmail.com>
2018-07-25 21:02:46 +0800

    docs(tabs): 完善示例

--- src/pages/navigation/tabs/index.js
+++ src/pages/navigation/tabs/index.js
@@ -19,6 +19,7 @@ export default class Index extends Taro.Component {
     })
   }
   render () {
+    const { current } = this.state
     return (
       <View className='example__body'>
         <View className='item'>
@@ -28,10 +29,10 @@ export default class Index extends Taro.Component {
           <AtTabs scroll current={1} tabList={[{ title: '标签页1' }, { title: '很长很长很长很长的标签页2' }, { title: '标签页3' }, { title: '标签页4' }, { title: '标签页5' }, { title: '标签页7' }, { title: '标签页8' }, { title: '标签页9' }]} />
         </View>
         <View className='item'>
-          <AtTabs onClick={this.handleClick.bind(this)} color='#f759ab' scroll current={this.state.current} tabList={[{ title: '标签页1' }, { title: '很长很长很长很长的标签页2' }, { title: '标签页3' }]} />
-          {this.state.current === 0 ? <View className='tab-content'>标签1的内容</View> : null}
-          {this.state.current === 1 ? <View className='tab-content'>标签2的内容</View> : null}
-          {this.state.current === 2 ? <View className='tab-content'>标签3的内容</View> : null}
+          <AtTabs onClick={this.handleClick.bind(this)} color='#f759ab' scroll current={current} tabList={[{ title: '标签页1' }, { title: '很长很长很长很长的标签页2' }, { title: '标签页3' }]} />
+          {current === 0 ? <View className='tab-content'>标签1的内容</View> : null}
+          {current === 1 ? <View className='tab-content'>标签2的内容</View> : null}
+          {current === 2 ? <View className='tab-content'>标签3的内容</View> : null}
         </View>
       </View>
     )
