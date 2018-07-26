beeafd7be73a64e1509ce741c0fca3b70d5bda30
jimczj <jimczj@gmail.com>
2018-07-26 17:12:02 +0800

    feat(tab-bar): 新增tab-bar组件

--- src/pages/navigation/tab-bar/index.js
+++ src/pages/navigation/tab-bar/index.js
@@ -23,17 +23,16 @@ export default class Index extends Taro.Component {
     return (
       <View className='example__body'>
         <View className='item'>
-          <AtTabBar tabList={[{ title: '标签页1' }, { title: '标签页2' }]} />
+          <AtTabBar tabList={[{ title: '标签页1', dot: true }, { title: '标签页2', count: 8 }]} />
         </View>
         <View className='item'>
-          <AtTabBar current={1} tabList={[{ title: '标签页1' }, { title: '很长很长很长很长的标签页2' }, { title: '标签页3' }, { title: '标签页4' }, { title: '标签页5' }, { title: '标签页7' }, { title: '标签页8' }, { title: '标签页9' }]} />
-        </View>
-        <View className='item'>
-          <AtTabBar onClick={this.handleClick.bind(this)} color='#f759ab' scroll current={current} tabList={[{ title: '标签页1' }, { title: '很长很长很长很长的标签页2' }, { title: '标签页3' }]} />
+          <AtTabBar backgroundColor='#B2B2B2' onClick={this.handleClick.bind(this)} selectedColor='#6190E8' fontSize='10' current={current} tabList={[{ title: '标签页1', iconType: 'computer', dot: true, selectedIconType: 'computer_fill', count: 8 }, { title: '标签页2', iconType: 'computer', dot: true }, { title: '标签页3', iconType: 'computer', count: '99' }]} />
           {current === 0 ? <View className='tab-content'>标签1的内容</View> : null}
           {current === 1 ? <View className='tab-content'>标签2的内容</View> : null}
           {current === 2 ? <View className='tab-content'>标签3的内容</View> : null}
         </View>
+        <AtTabBar fixed onClick={this.handleClick.bind(this)} selectedColor='#6190E8' fontSize='10' current={current} tabList={[{ title: '标签页1', iconType: 'computer', dot: true, selectedIconType: 'computer_fill', count: 8 }, { title: '标签页2', iconType: 'computer', dot: true }, { title: '标签页3', iconType: 'computer', count: '99' }]} />
+
       </View>
     )
   }
