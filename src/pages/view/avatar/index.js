9cc2ca8c0abc85902fb455432e857da1e0900c7d
koppthe <kopptsoft@gmail.com>
2018-07-27 15:22:59 +0800

    feat(doc): 重构视图展示页

--- src/pages/view/avatar/index.js
+++ src/pages/view/avatar/index.js
@@ -24,57 +24,87 @@ export default class AvatarPage extends Taro.Component {
   render () {
     return (
       <View className='page'>
-
-        <View className='example'>
-          <View className='example-item'>
-            <AtAvatar circle size='small' image='https://jdc.jd.com/img/200'></AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar circle size='large' image='https://jdc.jd.com/img/200'></AtAvatar>
-          </View>
-
+        {/* S Header */}
+        <View className='doc-header'>
+          <View className='doc-header__title'>头像</View>
         </View>
+        {/* E Header */}
 
-        <View className='example'>
-          <View className='example-item'>
-            <AtAvatar size='small' image='https://jdc.jd.com/img/200'></AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar image='https://jdc.jd.com/img/200'></AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar size='large' image='https://jdc.jd.com/img/200'></AtAvatar>
+        {/* S Body */}
+        <View className='doc-body'>
+          {/* 圆形头像 */}
+          <View className='panel'>
+            <View className='panel__title'>圆形头像</View>
+            <View className='panel__content'>
+              <View className='example'>
+                <View className='example-item'>
+                  <AtAvatar circle size='small' image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar circle size='large' image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+              </View>
+            </View>
           </View>
 
+          {/* 圆角矩形头像 */}
+          <View className='panel'>
+            <View className='panel__title'>圆角矩形头像</View>
+            <View className='panel__content'>
+              <View className='example'>
+                <View className='example-item'>
+                  <AtAvatar size='small' image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar size='large' image='https://jdc.jd.com/img/200'></AtAvatar>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* 圆形头像（支持文本） */}
+          <View className='panel'>
+            <View className='panel__title'>圆形头像（支持文本）</View>
+            <View className='panel__content'>
+              <View className='example'>
+                <View className='example-item'>
+                  <AtAvatar circle size='small' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar circle image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar circle size='large' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* 圆角矩形头像（支持文本） */}
+          <View className='panel'>
+            <View className='panel__title'>圆角矩形头像（支持文本）</View>
+            <View className='panel__content'>
+              <View className='example'>
+                <View className='example-item'>
+                  <AtAvatar size='small' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+                <View className='example-item'>
+                  <AtAvatar size='large' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
+                </View>
+              </View>
+            </View>
+          </View>
         </View>
-
-        <View className='example'>
-          <View className='example-item'>
-            <AtAvatar circle size='small' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar circle image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar circle size='large' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-        </View>
-
-        <View className='example'>
-          <View className='example-item'>
-            <AtAvatar size='small' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-          <View className='example-item'>
-            <AtAvatar size='large' image='https://jdc.jd.com/img/200'>凹</AtAvatar>
-          </View>
-        </View>
-
+        {/* E Body */}
       </View>
     )
   }
