c05b583268ecd5578acedef286f02bc07401d7d3
jimczj <jimczj@gmail.com>
2018-07-31 15:07:55 +0800

    refactor(segmented-control): 重命名props

--- src/pages/navigation/segmented-control/index.js
+++ src/pages/navigation/segmented-control/index.js
@@ -13,30 +13,47 @@ export default class Index extends Taro.Component {
       current: 0,
     }
   }
-  handleClick (detail) {
+  handleClick (value) {
     this.setState({
-      current: detail.value
+      current: value
     })
   }
   render () {
     const { current } = this.state
-    const tabList1 = [{ title: '标签页1' }, { title: '标签页2' }]
-    const tabList2 = [{ title: '标签页1' }, { title: '标签页2' }, { title: '标签页3' }]
-    const tabList3 = [{ title: '标签页1' }, { title: '标签页2' }, { title: '标签页3' }, { title: '标签页4' }]
+    const tabList1 = ['标签页1', '标签页2' ]
+    const tabList2 = ['标签页1', '标签页2', '标签页3']
     return (
-      <View className='example__body'>
-        <View className='item'>
-          <AtSegmentedControl disabled tabList={tabList1} />
+      <View className='page'>
+        {/* S Header */}
+        <View className='doc-header'>
+          <View className='doc-header__title'>分段器</View>
         </View>
-        <View className='item'>
-          <AtSegmentedControl onClick={this.handleClick.bind(this)} selectedColor='#6190E8' current={current} tabList={tabList2} />
-          {current === 0 ? <View className='tab-content'>标签1的内容</View> : null}
-          {current === 1 ? <View className='tab-content'>标签2的内容</View> : null}
-          {current === 2 ? <View className='tab-content'>标签3的内容</View> : null}
+        {/* E Header */}
+        {/* S Body */}
+        <View className='doc-body'>
+          <View className='panel'>
+            <View className='panel__title'>基础</View>
+            <View className='panel__content'>
+              <View>
+                <AtSegmentedControl onClick={this.handleClick.bind(this)} selectedColor='#6190E8' current={current} values={tabList2} />
+                {current === 0 ? <View className='tab-content'>标签1的内容</View> : null}
+                {current === 1 ? <View className='tab-content'>标签2的内容</View> : null}
+                {current === 2 ? <View className='tab-content'>标签3的内容</View> : null}
+              </View>
+            </View>
+            <View className='panel__title'>禁用</View>
+            <View className='panel__content'>
+              <View><AtSegmentedControl disabled values={tabList1} /></View>
+            </View>
+            <View className='panel__title'>自定义颜色</View>
+            <View className='panel__content'>
+              <View>
+                <AtSegmentedControl onClick={this.handleClick.bind(this)} selectedColor='#FF4949' fontSize='12' current={current} values={tabList2} />
+              </View>
+            </View>
+          </View>
         </View>
-        <View className='item'>
-          <AtSegmentedControl onClick={this.handleClick.bind(this)} selectedColor='#FF4949' fontSize='12' current={current} tabList={tabList3} />
-        </View>
+        {/* E Body */}
       </View>
     )
   }
