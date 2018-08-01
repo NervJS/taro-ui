53a064b29e4573cc30ef0d3a64a71549050cccf3
jimczj <jimczj@gmail.com>
2018-08-01 20:50:27 +0800

    refactor(radio): 修改props

--- src/components/radio/index.js
+++ src/components/radio/index.js
@@ -8,18 +8,19 @@ import './index.scss'
  * @author:chenzeji
  * @description 单选框
  * @prop value {String} 当前单选框值
- * @prop options {Array} 选项列表 eg:['苹果']
+ * @prop options {Array} 选项列表 eg:[{label:'苹果',value:'apple'}]
  * @prop onChange {Function} 监听事件改变函数
  */
 class AtRadio extends Taro.Component {
-  handleClick (option) {
-    this.props.onClick && this.props.onClick({ value: option })
+  handleClick (value) {
+    this.props.onClick(value, ...arguments)
   }
   render () {
+    const { options, value } = this.props
     return <View className='at-radio'>
       {
-        this.props.options.map(option => <View key={option} onClick={this.handleClick.bind(this, option)} className={option === this.props.value ? 'at-radio__option at-radio__option--selected' : 'at-radio__option'} >
-          <View className='at-radio__title'>{option.toString()}</View>
+        options.map(option => <View key={option} onClick={this.handleClick.bind(this, option.value)} className={option.value === value ? 'at-radio__option at-radio__option--selected' : 'at-radio__option'} >
+          <View className='at-radio__title'>{option.label}</View>
           <View className='at-radio__icon'>
             <AtIcon value='right' size='20' color='#6190e8' />
           </View>
