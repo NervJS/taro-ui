e1cefbf4e109e28b497fc44987995491f8dd91b5
koppthe <kopptsoft@gmail.com>
2018-08-02 22:22:09 +0800

    docs(Article): 完善 Article 展示页

--- src/pages/view/article/index.js
+++ src/pages/view/article/index.js
@@ -1,12 +1,13 @@
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import AtArticle from '../../../components/article/index'
+import DocsHeader from '../../components/doc-header'
 
 import './index.scss'
 
 export default class ArticlePage extends Taro.Component {
   config = {
-    navigationBarTitleText: 'Article Page'
+    navigationBarTitleText: 'Taro UI'
   }
 
   constructor () {
@@ -24,8 +25,20 @@ export default class ArticlePage extends Taro.Component {
   render () {
     return (
       <View className='page'>
+        {/* S Header */}
+        <DocsHeader title='Article 文章'></DocsHeader>
+        {/* E Header */}
 
-        <AtArticle></AtArticle>
+        {/* S Body */}
+        <View className='doc-body'>
+          <View className='panel'>
+            <View className='panel__title'>示例</View>
+            <View className='panel__content'>
+              <AtArticle></AtArticle>
+            </View>
+          </View>
+        </View>
+        {/* E Body */}
       </View>
     )
   }
