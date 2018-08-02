f55e170ba17c0743a70b0ab2befe8f6180328fbf
jimczj <jimczj@gmail.com>
2018-08-02 15:50:11 +0800

    fix(form): 修复style类型报错

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,10 +2,14 @@ import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import './index.scss'
 
-export default class AtForm extends Taro.Component {
+class AtForm extends Taro.Component {
   render () {
     return <View className='at-form' style={this.props.style}>
       {this.props.children}
     </View>
   }
 }
+AtForm.defaultProps = {
+  style: ''
+}
+export default AtForm
