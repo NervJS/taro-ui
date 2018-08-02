dc0e1885f919033ab15d4f6720d0fa35a4a49596
koppthe <kopptsoft@gmail.com>
2018-08-02 22:27:13 +0800

    docs(ActionSheet): 完善 ActionSheet 展示页

--- src/pages/action/action-sheet/index.js
+++ src/pages/action/action-sheet/index.js
@@ -1,21 +1,22 @@
 /* eslint taro/custom-component-children: 0 */
 import Taro from '@tarojs/taro'
-import { View, Text, Button } from '@tarojs/components'
+import { View, Button } from '@tarojs/components'
 
 import AtActionSheet from '../../../components/action-sheet'
 import AtActionSheetItem from '../../../components/action-sheet/body/item/index'
+import DocsHeader from '../../components/doc-header'
 
 import './index.scss'
 
 export default class ActionSheetPage extends Taro.Component {
   config = {
-    navigationBarTitleText: 'ActionSheet Page'
+    navigationBarTitleText: 'Taro UI'
   }
 
   constructor () {
     super(...arguments)
     this.state = {
-      isOpened: true
+      isOpened: false
     }
   }
 
@@ -27,15 +28,25 @@ export default class ActionSheetPage extends Taro.Component {
   render () {
     const { isOpened } = this.state
     return (
-      <View className='action-sheet__page'>
-        <View className='example'>
-          <View className='example__header'>
-            <Text className='example__header-title'>真实案例</Text>
-          </View>
-          <View className='example__body'>
-            <Button onClick={this.handleClick}>打开ActionSheet</Button>
+      <View className='page'>
+        {/* S Header */}
+        <DocsHeader title='ActionSheet 操作面板'></DocsHeader>
+        {/* E Header */}
+
+        {/* S Body */}
+        <View className='doc-body'>
+          {/* 圆形头像 */}
+          <View className='panel'>
+            <View className='panel__title'>示例</View>
+            <View className='panel__content'>
+              <View className='example-item'>
+                <Button onClick={this.handleClick}>打开 ActionSheet</Button>
+              </View>
+            </View>
           </View>
         </View>
+        {/* E Body */}
+
         <AtActionSheet title='标题' cancleText='取消' isOpened={isOpened}>
           <AtActionSheetItem>这是啥??</AtActionSheetItem>
           <AtActionSheetItem>这是啥</AtActionSheetItem>
