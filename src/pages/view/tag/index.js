ed217458d2c893315037620856b1c2ce90ca6241
koppthe <kopptsoft@gmail.com>
2018-08-02 22:15:25 +0800

    docs(Tag): 完善 Tag 展示页

--- src/pages/view/tag/index.js
+++ src/pages/view/tag/index.js
@@ -2,6 +2,7 @@ import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
 import AtTag from '../../../components/tag/index'
+import DocsHeader from '../../components/doc-header'
 
 import './index.scss'
 
@@ -26,9 +27,7 @@ export default class TagPage extends Taro.Component {
     return (
       <View className='page'>
         {/* S Header */}
-        <View className='doc-header'>
-          <View className='doc-header__title'>标签</View>
-        </View>
+        <DocsHeader title='Tag 标签'></DocsHeader>
         {/* E Header */}
 
         {/* S Body */}
@@ -37,17 +36,17 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>常规标签</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag circle onClick={this.onClick.bind(this)}>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag circle active>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag active>标签</AtTag>
                 </View>
               </View>
@@ -58,17 +57,17 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>选中态</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary' circle>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary'>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary' circle active>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary' active>标签</AtTag>
                 </View>
               </View>
@@ -79,11 +78,11 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>不可点击态</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary' circle disabled>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag type='primary' disabled>标签</AtTag>
                 </View>
               </View>
@@ -94,17 +93,17 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>常规标签（小）</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' circle onClick={this.onClick.bind(this)}>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small'>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' circle active>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' active>标签</AtTag>
                 </View>
               </View>
@@ -115,17 +114,17 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>选中态（小）</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary' circle>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary'>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary' circle active>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary' active>标签</AtTag>
                 </View>
               </View>
@@ -136,11 +135,11 @@ export default class TagPage extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>不可点击态（小）</View>
             <View className='panel__content'>
-              <View className='example'>
-                <View className='example-item'>
+              <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary' circle disabled>标签</AtTag>
                 </View>
-                <View className='example-item'>
+                <View className='subitem'>
                   <AtTag size='small' type='primary' disabled>标签</AtTag>
                 </View>
               </View>
