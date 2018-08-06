db332a833f10bbea5f8ca924c2f174fde788ac98
koppthe <kopptsoft@gmail.com>
2018-08-06 14:34:02 +0800

    docs(首页): 替换移动端首页的图标

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,13 @@
 import Taro from '@tarojs/taro'
 import { View, Image, Text } from '@tarojs/components'
+
 import logoImg from '../../assets/images/logo_taro.png'
+import iconBasic from '../../assets/images/icon-list-basic.png'
+import iconView from '../../assets/images/icon-list-view.png'
+import iconAction from '../../assets/images/icon-list-action.png'
+import iconForm from '../../assets/images/icon-list-form.png'
+import iconLayout from '../../assets/images/icon-list-layout.png'
+import iconNavigation from '../../assets/images/icon-list-navigation.png'
 
 import './index.scss'
 
@@ -17,32 +24,38 @@ export default class Index extends Taro.Component {
         {
           id: 'Basic',
           title: '基础',
-          content: '包含颜色、文本、图标'
+          content: '包含颜色、文本、图标',
+          icon: iconBasic
         },
         {
           id: 'View',
           title: '视图',
-          content: '6个组件'
+          content: '6个组件',
+          icon: iconView
         },
         {
           id: 'Action',
           title: '操作反馈',
-          content: '5个组件'
+          content: '5个组件',
+          icon: iconAction
         },
         {
           id: 'Form',
           title: '表单',
-          content: '7个组件'
+          content: '7个组件',
+          icon: iconForm
         },
         {
           id: 'Layout',
           title: '布局',
-          content: '5个组件'
+          content: '5个组件',
+          icon: iconLayout
         },
         {
           id: 'Navigation',
           title: '导航',
-          content: '6个组件'
+          content: '6个组件',
+          icon: iconNavigation
         }
       ]
     }
@@ -75,7 +88,7 @@ export default class Index extends Taro.Component {
               onClick={this.gotoPanel}
             >
               <View className='module-list__icon'>
-                <Text className='at-icon at-icon-list'></Text>
+                <Image src={item.icon} className='img' mode='widthFix' />
               </View>
               <View className='module-list__info'>
                 <View className='title'>{item.title}</View>
