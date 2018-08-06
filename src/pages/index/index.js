b56be231a8fbb6caa6dc34eaee4dfebec5505403
koppthe <kopptsoft@gmail.com>
2018-08-06 14:48:55 +0800

    docs(分类页): 替换分类页图标

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -24,37 +24,37 @@ export default class Index extends Taro.Component {
         {
           id: 'Basic',
           title: '基础',
-          content: '包含颜色、文本、图标',
+          content: '包含颜色、文本、图标等',
           icon: iconBasic
         },
         {
           id: 'View',
           title: '视图',
-          content: '6个组件',
+          content: '包含通告栏、标签、徽标等',
           icon: iconView
         },
         {
           id: 'Action',
           title: '操作反馈',
-          content: '5个组件',
+          content: '包含对话框、进度条、动作面板等',
           icon: iconAction
         },
         {
           id: 'Form',
           title: '表单',
-          content: '7个组件',
+          content: '包含输入框、单选框、复选框等',
           icon: iconForm
         },
         {
           id: 'Layout',
           title: '布局',
-          content: '5个组件',
+          content: '包含列表、浮层、卡片等',
           icon: iconLayout
         },
         {
           id: 'Navigation',
           title: '导航',
-          content: '6个组件',
+          content: '包含标签栏、导航栏、时间轴等',
           icon: iconNavigation
         }
       ]
