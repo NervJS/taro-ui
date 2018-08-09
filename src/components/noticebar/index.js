d2d98a921972b30effc23405008d05563b85508f
koppthe <kopptsoft@gmail.com>
2018-08-09 21:20:55 +0800

    feat(NoticeBar): 优化通告栏样式

--- src/components/noticebar/index.js
+++ src/components/noticebar/index.js
@@ -1,7 +1,6 @@
-
 import PropTypes from 'prop-types'
 import Taro from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import AtIcon from '../icon/index'
 
 import './index.scss'
@@ -38,10 +37,10 @@ export default class AtNoticebar extends Taro.Component {
       single,
       icon,
     } = this.props
-    let _moreText = this.props.moreText
-    if (!_moreText) _moreText = '查看详情'
-    console.log('moreText===')
     const rootClassName = ['at-noticebar']
+    let _moreText = this.props.moreText
+
+    if (!_moreText) _moreText = '查看详情'
     if (moreUrl) rootClassName.push('at-noticebar--more')
     if (single) rootClassName.push('at-noticebar--single')
 
@@ -50,9 +49,9 @@ export default class AtNoticebar extends Taro.Component {
       <View className={rootClassName}>
         {close && <View className='at-noticebar__close' onClick={this.onClose.bind(this)}><AtIcon value='close' size='16' color='#ccc'></AtIcon></View>}
         <View className='at-noticebar__content'>
-          {icon && <AtIcon value={icon} size='16'></AtIcon>}{this.props.children}
+          {icon && <View className='at-noticebar__content-icon'><AtIcon value={icon} size='16'></AtIcon></View>}{this.props.children}
         </View>
-        {moreUrl && <View className='at-noticebar__more' onClick={this.onGotoMore.bind(this)}>{_moreText}<View className='at-noticebar__more-icon'><AtIcon value='chevron-right' size='16'></AtIcon></View></View>}
+        {moreUrl && <View className='at-noticebar__more' onClick={this.onGotoMore.bind(this)}><Text className='text'>{_moreText}</Text><View className='at-noticebar__more-icon'><AtIcon value='chevron-right' size='16'></AtIcon></View></View>}
       </View>
     )
   }
