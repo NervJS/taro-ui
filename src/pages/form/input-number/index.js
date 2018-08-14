5acbef21fc22bc9509fcc048424ab8cec965ce44
koppthe <kopptsoft@gmail.com>
2018-08-14 11:18:26 +0800

    feat(Form): 修改表单组件的样式

--- src/pages/form/input-number/index.js
+++ src/pages/form/input-number/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-direct-mutation-state */
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import AtInputNumber from '../../../components/input-number/index'
@@ -18,8 +17,9 @@ export default class Index extends Taro.Component {
     }
   }
   handleNumberChange (stateName, value) {
-    this.state[stateName] = value
-    this.setState()
+    this.setState({
+      [stateName]: value
+    })
   }
   render () {
     return (
@@ -30,31 +30,35 @@ export default class Index extends Taro.Component {
 
         {/* S Body */}
         <View className='doc-body'>
+          {/* 基础用法 */}
           <View className='panel'>
-            <View className='panel__title'>基础数字输入框</View>
+            <View className='panel__title'>基础用法</View>
             <View className='panel__content'>
-              <View className='example--item'>
+              <View className='example-item'>
+                <View className='example-item__desc'>min=0, max=10</View>
                 <AtInputNumber min={0} max={10} step={1} value={this.state.number1} onChange={this.handleNumberChange.bind(this, 'number1')} />
               </View>
             </View>
           </View>
 
+          {/* 小数 */}
           <View className='panel'>
             <View className='panel__title'>小数</View>
             <View className='panel__content'>
-              <View className='example--item'>
+              <View className='example-item'>
+                <View className='example-item__desc'>step=0.1</View>
                 <AtInputNumber min={0} max={5} step={0.1} value={this.state.number2} onChange={this.handleNumberChange.bind(this, 'number2')} />
               </View>
             </View>
           </View>
 
+          {/* 禁用状态 */}
           <View className='panel'>
-            <View className='panel__title'>禁止状态</View>
+            <View className='panel__title'>禁用状态</View>
             <View className='panel__content'>
-              <View className='example--item'>
+              <View className='example-item'>
                 <AtInputNumber disabled min={0} max={10} step={1} value={this.state.number3} onChange={this.handleNumberChange.bind(this, 'number3')} />
               </View>
-
             </View>
           </View>
         </View>
