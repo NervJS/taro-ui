dc325ae8d956f8390ea3452566261608d1c5d738
koppthe <kopptsoft@gmail.com>
2018-08-23 20:22:21 +0800

    docs: 优化展示页面

--- src/pages/navigation/tabbar/index.js
+++ src/pages/navigation/tabbar/index.js
@@ -12,18 +12,21 @@ export default class Index extends Taro.Component {
   constructor () {
     super(...arguments)
     this.state = {
-      current: 0,
+      current1: 0,
+      current2: 0,
+      current3: 0,
+      current4: 0,
     }
   }
 
-  handleClick (value) {
+  handleClick (num, value) {
     this.setState({
-      current: value
+      [`current${num}`]: value
     })
   }
 
   render () {
-    const { current } = this.state
+    const { current1, current2, current3, current4 } = this.state
     const tabList1 = [{ title: '待办事项', text: 8 }, { title: '拍照' }, { title: '通讯录', dot: true }]
     const tabList2 = [{ title: '待办事项', iconType: 'bullet-list', text: 'new' }, { title: '拍照', iconType: 'camera' }, { title: '文件夹', iconType: 'folder', text: '100', max: '99' }]
 
@@ -35,25 +38,25 @@ export default class Index extends Taro.Component {
           <View className='panel'>
             <View className='panel__title'>文本标签栏</View>
             <View className='panel__content no-padding'>
-              <AtTabBar tabList={tabList1} onClick={this.handleClick.bind(this)} current={current} />
+              <AtTabBar tabList={tabList1} onClick={this.handleClick.bind(this, 1)} current={current1} />
             </View>
           </View>
           <View className='panel'>
             <View className='panel__title'>图标文本标签栏</View>
             <View className='panel__content no-padding'>
-              <AtTabBar tabList={tabList2} onClick={this.handleClick.bind(this)} current={current} />
+              <AtTabBar tabList={tabList2} onClick={this.handleClick.bind(this, 2)} current={current2} />
             </View>
           </View>
           <View className='panel'>
             <View className='panel__title'>自定义图标颜色、字体颜色、背景颜色</View>
             <View className='panel__content no-padding'>
-              <AtTabBar backgroundColor='#FAFBFC' color='#ea6bb8' selectedColor='#e64340' tabList={tabList2} onClick={this.handleClick.bind(this)} current={current} />
+              <AtTabBar backgroundColor='#FAFBFC' color='#ea6bb8' selectedColor='#e64340' tabList={tabList2} onClick={this.handleClick.bind(this, 3)} current={current3} />
             </View>
           </View>
           <View className='panel'>
             <View className='panel__title'>固定底部</View>
             <View className='panel__content no-padding' style='padding-bottom: 24px;'>
-              <AtTabBar fixed tabList={tabList2} onClick={this.handleClick.bind(this)} current={current} />
+              <AtTabBar fixed tabList={tabList2} onClick={this.handleClick.bind(this, 4)} current={current4} />
             </View>
           </View>
         </View>
