d85d93decb9b965b03c2c2549327644afe08ec3a
jimczj <jimczj@gmail.com>
2018-08-24 13:25:31 +0800

    fix(button): small 状态的 loading 大小

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,23 +1,42 @@
 import Taro from '@tarojs/taro'
+import PropTypes from 'prop-types'
 import { View } from '@tarojs/components'
 import './index.scss'
 
 class AtLoading extends Taro.Component {
   render () {
-    const style = {
-      'border': `1px solid ${this.props.color}`,
-      'border-color': `${this.props.color} transparent transparent transparent`
+    const { color, size } = this.props
+    const sizeStyle = {
+      width: `${size}px`,
+      height: `${size}px`
     }
+    const colorStyle = {
+      'border': `1px solid ${color}`,
+      'border-color': `${color} transparent transparent transparent`
+    }
+    const ringStyle = Object.assign({}, colorStyle, sizeStyle)
     return (
-      <View className='at-loading'>
-        <View className='at-loading__ring' style={style}></View>
-        <View className='at-loading__ring' style={style}></View>
-        <View className='at-loading__ring' style={style}></View>
+      <View className='at-loading' style={sizeStyle}>
+        <View className='at-loading__ring' style={ringStyle}></View>
+        <View className='at-loading__ring' style={ringStyle}></View>
+        <View className='at-loading__ring' style={ringStyle}></View>
       </View>
     )
   }
 }
+
 AtLoading.defaultProps = {
+  size: '18',
   color: '#fff'
+}
+AtLoading.propTypes = {
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  color: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ])
 }
 export default AtLoading
