80efa61effd186fde7eeb0bdef2da25117f7b741
Limichange <limichange@hotmail.com>
2018-09-04 10:52:44 +0800

    fix(types): AtSwitchProps

--- @types/switch.d.ts
+++ @types/switch.d.ts
@@ -2,17 +2,17 @@ import { MouseEvent, ComponentClass } from 'react'
 import { BaseEventFunction, BaseEvent } from '@tarojs/components/types/common'
 
 export interface AtSwitchProps {
-  title: string
+  title?: string
 
-  color: string
+  color?: string
 
-  checked: boolean
+  checked?: boolean
 
-  border: boolean
+  border?: boolean
 
-  disabled: boolean
+  disabled?: boolean
 
-  onChange: (value: boolean) => void
+  onChange?: (value: boolean) => void
 }
 
 declare const AtSwitch: ComponentClass<AtSwitchProps>
