ed73c918307e869f7e3552b4d7e376273becfceb
jimczj <jimczj@gmail.com>
2018-09-13 19:15:03 +0800

    feat(icon): resolved #26 自定义 icon

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,15 @@ module.exports = {
   },
   defineConstants: {
   },
-  weapp: {},
+  weapp: {
+    postcss: {
+      // 小程序端样式引用本地资源内联
+      url: {
+        enable: true,
+        limit: 102400000000
+      }
+    }
+  },
   h5: {
     publicPath: './',
   }
--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -6,38 +6,50 @@ import AtComponent from '../../common/component'
 import './index.scss'
 
 export default class AtIcon extends AtComponent {
+  static defaultProps = {
+    prefixClass: 'at-icon',
+    value: '',
+    color: '',
+    size: 24,
+    onClick: () => { },
+  }
+
+  static propTypes = {
+    prefixClass: PropTypes.string,
+    value: PropTypes.string,
+    color: PropTypes.string,
+    size: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ]),
+    onClick: PropTypes.func,
+  }
+
   handleClick () {
     this.props.onClick(...arguments)
   }
 
   render () {
-    const { value = '', size, color } = this.props
+    const {
+      prefixClass,
+      value,
+      size,
+      color
+    } = this.props
 
     const rootStyle = {
       fontSize: `${size}px`,
-      color,
+      color
     }
+    const rootClassName = [prefixClass, `${prefixClass}-${value}`]
 
-    const rootClassName = ['at-icon', `at-icon-${value}`]
-    // console.log('value====', value)
     return (
-      <Text className={rootClassName} style={rootStyle} onClick={this.handleClick.bind(this)}></Text>
+      <Text
+        className={this.getClassName(rootClassName)}
+        style={rootStyle}
+        onClick={this.handleClick.bind(this)}
+      >
+      </Text>
     )
   }
 }
-AtIcon.defaultProps = {
-  value: '',
-  color: '',
-  size: 24,
-  onClick: () => {},
-}
-AtIcon.propTypes = {
-  value: PropTypes.string,
-  color: PropTypes.string,
-  size: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-  onClick: PropTypes.func,
-}
-
