b1e1d444163ee0162600929f76d702e7c0dbe8e9
koppthe <kopptsoft@gmail.com>
2018-09-28 21:15:50 +0800

    docs: 添加 Swiper 和 Picker 文档

---
+++ docs/view/Picker/index.jsx
@@ -0,0 +1,10 @@
+import * as Nerv from 'nervjs'
+import PickerDoc from '@md/picker.md'
+
+class PickerView extends Nerv.Component {
+  render () {
+    return <PickerDoc />
+  }
+}
+
+export default PickerView
