63acb61e205437d90c522ca2589c142858ba075d
jimczj <jimczj@gmail.com>
2018-09-28 14:48:22 +0800

    feat(search-bar): 新增组件

---
+++ src/components/search-bar/index.js
@@ -0,0 +1,174 @@
+import Taro from '@tarojs/taro'
+import { View, Text, Input } from '@tarojs/components'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+import AtIcon from '../../components/icon/index'
+import AtComponent from '../../common/component'
+import './index.scss'
+
+const defaultFunc = () => { }
+
+export default class AtSearchBar extends AtComponent {
+  constructor (props) {
+    super(...arguments)
+    this.state = {
+      isFocus: props.focus
+    }
+  }
+
+  static defaultProps = {
+    value: '',
+    placeholder: '',
+    maxlength: 120,
+    fixed: false,
+    focus: false,
+    disabled: false,
+    showActionButton: false,
+    actionName: '取消',
+    onChange: defaultFunc,
+    onFocus: defaultFunc,
+    onBlur: defaultFunc,
+    onConfirm: defaultFunc,
+  }
+
+  static propTypes = {
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    maxlength: PropTypes.number,
+    fixed: PropTypes.bool,
+    focus: PropTypes.bool,
+    disabled: PropTypes.bool,
+    showActionButton: PropTypes.bool,
+    actionName: PropTypes.string,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    onConfirm: PropTypes.func,
+    onActionClick: PropTypes.func
+  }
+
+  handleFocus (e) {
+    this.setState({
+      isFocus: true
+    })
+    this.props.onFocus(e, ...arguments)
+  }
+
+  handleBlur (e) {
+    this.setState({
+      isFocus: false
+    })
+    this.props.onBlur(e, ...arguments)
+  }
+
+  handleChange (e) {
+    this.props.onChange(e.target.value, e, ...arguments)
+  }
+
+  handleClear (e) {
+    this.props.onChange('', e, ...arguments)
+  }
+
+  handleConfirm (e) {
+    this.props.onConfirm(e, ...arguments)
+  }
+
+  handleActionClick (e) {
+    if (!this.props.onActionClick) {
+      this.setState({
+        isFocus: false
+      })
+      this.handleClear(e)
+    } else {
+      this.props.onActionClick(e)
+    }
+  }
+
+  render () {
+    const {
+      value,
+      placeholder,
+      maxlength,
+      fixed,
+      focus,
+      disabled,
+      showActionButton,
+      actionName,
+    } = this.props
+    const { isFocus } = this.state
+
+    const placeholderStyle = {}
+    const actionStyle = {}
+    if (isFocus || (!isFocus && value)) {
+      placeholderStyle.width = `${(placeholder.length + 2) * 14}px`
+      actionStyle.opacity = 1
+      actionStyle.marginRight = `0`
+    } else if (!isFocus && !value) {
+      placeholderStyle.width = '100%'
+      actionStyle.opacity = 0
+      actionStyle.marginRight = `-${((actionName.length) * 14) + 7}px`
+    }
+    if (showActionButton) {
+      actionStyle.opacity = 1
+      actionStyle.marginRight = `0`
+    }
+    return (
+      <View
+        className={
+          classNames({
+            'at-search-bar': true,
+            'at-search-bar--fixed': fixed
+          })
+        }
+      >
+        <View className='at-search-bar__container'>
+          <View
+            className='at-search-bar__placeholder_wrap'
+            style={placeholderStyle}
+          >
+            <AtIcon value='search' size='15' color='#999' />
+            <Text
+              className='at-search-bar__placeholder'
+              style={
+                value.length
+                  ? 'visibility: hidden;'
+                  : 'visibility: visible;'
+              }
+            >{placeholder}</Text>
+          </View>
+          <Input
+            className='at-search-bar__input'
+            type='search'
+            value={value}
+            focus={focus}
+            disabled={disabled}
+            maxlength={maxlength}
+            onInput={this.handleChange.bind(this)}
+            onFocus={this.handleFocus.bind(this)}
+            onBlur={this.handleBlur.bind(this)}
+            onConfirm={this.handleConfirm.bind(this)}
+          />
+          <View
+            className='at-search-bar__clear'
+            style={
+              value.length
+                ? 'display: flex;'
+                : 'display: none;'
+            }
+            onTouchStart={this.handleClear.bind(this)}
+          >
+            <AtIcon value='close-circle' size='15' color='#999' />
+          </View>
+        </View>
+        <View
+          onClick={this.handleActionClick.bind(this)}
+          className='at-search-bar__action'
+          style={actionStyle}
+        >
+          {actionName}
+        </View>
+      </View>
+    )
+  }
+}
--- src/pages/panel/index.js
+++ src/pages/panel/index.js
@@ -156,6 +156,10 @@ export default class PanelBasic extends Component {
           {
             id: 'Slider',
             name: '滑动条'
+          },
+          {
+            id: 'Search-Bar',
+            name: '搜索框'
           }
         ],
         'layout': [
