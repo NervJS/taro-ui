01ed42ff07c42098310990b4b6f2b69d2e3fd306
hejason <szhejason@gmail.com>
2018-11-13 15:54:06 +0800

    docs(calendar): 更新日历文档

--- docs/page-route.js
+++ docs/page-route.js
@@ -41,5 +41,6 @@ export default {
   pagination: 'navigation/pagination',
   segmentedcontrol: 'navigation/segmented-control',
   tabbar: 'navigation/tabbar',
-  tabs: 'navigation/tabs'
+  tabs: 'navigation/tabs',
+  calendar: 'advanced/calendar'
 }
---
+++ docs/view/Calendar/index.jsx
@@ -0,0 +1,12 @@
+import * as Nerv from 'nervjs'
+import CalendarDoc from '@md/calendar.md'
+
+class CalendarView extends Nerv.Component {
+  render() {
+    return (
+      <CalendarDoc />
+    )
+  }
+}
+
+export default CalendarView
