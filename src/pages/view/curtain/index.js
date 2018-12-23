f9cedb1c875f7b8335c793da6136458e1f4be9b3
koppt <kopptsoft@gmail.com>
2018-12-23 18:15:03 +0800

    feat(Curtain): 重构幕帘样式，增加多 position 支持

--- src/pages/view/curtain/index.js
+++ src/pages/view/curtain/index.js
@@ -51,19 +51,11 @@ export default class TagPage extends Taro.Component {
             onClose={this.onClose.bind(this)}
           >
             <Image
-              style='width:100%;height:250px'
+              style='width:100%'
+              mode='widthFix'
               src={curtainPng}
             />
           </AtCurtain>
-          {/* 右上关闭 */}
-          <View className='panel'>
-            <View className='panel__title'>右上关闭</View>
-            <View className='panel__content'>
-              <View className='example-item'>
-                <AtButton onClick={this.handleChange.bind(this, 'closeBtnPosition', 'top')}>右上关闭幕帘</AtButton>
-              </View>
-            </View>
-          </View>
 
           {/* 底部关闭 */}
           <View className='panel'>
@@ -71,6 +63,16 @@ export default class TagPage extends Taro.Component {
             <View className='panel__content'>
               <View className='example-item'>
                 <AtButton onClick={this.handleChange.bind(this, 'closeBtnPosition', 'bottom')}>底部关闭幕帘</AtButton>
+              </View>
+            </View>
+          </View>
+
+          {/* 右上关闭 */}
+          <View className='panel'>
+            <View className='panel__title'>右上关闭</View>
+            <View className='panel__content'>
+              <View className='example-item'>
+                <AtButton onClick={this.handleChange.bind(this, 'closeBtnPosition', 'top-right')}>右上关闭幕帘</AtButton>
               </View>
             </View>
           </View>
