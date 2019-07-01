e9a6bd00e7049fdc7a7c7f9ad63e192142a08382
aoxiang78 <ao_xiang@live.com>
2019-07-01 13:27:26 +0800

    fix tabbar type error

--- src/components/tab-bar/index.js
+++ src/components/tab-bar/index.js
@@ -122,8 +122,8 @@ export default class AtTabBar extends AtComponent {
             <View>
               <AtBadge
                 dot={(item.iconType || item.image) ? false : !!item.dot}
-                value={(item.iconType || item.image) ? '' : item.text}
-                maxValue={(item.iconType || item.image) ? '' : item.max}
+                value={(item.iconType || item.image) ? null : item.text}
+                maxValue={(item.iconType || item.image) ? null : item.max}
               >
                 <View className='at-tab-bar__title' style={titleStyle}>
                   {item.title}
