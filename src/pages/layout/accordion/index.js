9962f97ca07ff541098eee34449270c2e4bb14d5
AwesomeDevin <awesomedevin.deng@gmail.com>
2019-09-25 17:24:06 +0800

    feat: accordion支持note属性

--- src/pages/layout/accordion/index.js
+++ src/pages/layout/accordion/index.js
@@ -14,7 +14,8 @@ export default class CardPage extends Taro.Component {
     this.state = {
       value1: false,
       value2: true,
-      value3: false
+      value3: false,
+      value4: false,
     }
   }
 
@@ -25,7 +26,7 @@ export default class CardPage extends Taro.Component {
   }
 
   render () {
-    const { value1, value2, value3 } = this.state
+    const { value1, value2, value3, value4 } = this.state
 
     return (
       <View className='page'>
@@ -85,6 +86,34 @@ export default class CardPage extends Taro.Component {
               <View className='example-item'>
                 <AtAccordion
                   title='标题三'
+                  open={value4}
+                  icon={{ value: 'tags', color: '#77a1fd' }}
+                  onClick={this.onClick.bind(this, 'value3')}
+                >
+                  <AtList hasBorder={false}>
+                    <AtListItem
+                      title='标题文字'
+                      thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+                    />
+                    <AtListItem
+                      title='标题文字'
+                      note='描述信息'
+                      thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+                    />
+                  </AtList>
+                </AtAccordion>
+              </View>
+            </View>
+          </View>
+
+          {/* 包含描述信息 */}
+          <View className='panel'>
+            <View className='panel__title'>包含描述信息</View>
+            <View className='panel__content no-padding'>
+              <View className='example-item'>
+                <AtAccordion
+                  title='标题三'
+                  note='描述信息'
                   open={value3}
                   icon={{ value: 'tags', color: '#77a1fd' }}
                   onClick={this.onClick.bind(this, 'value3')}
