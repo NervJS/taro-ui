272ba71551c027061d2b99a5c0cc65ddbc43a82e
Garfield Lee <openpgp@550.moe>
2019-11-02 19:37:54 +0800

    refactor(fab): 使用 TypeScript 重构 AtFab

--- src/components/fab/index.tsx
+++ src/components/fab/index.tsx
@@ -1,15 +1,19 @@
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import PropTypes from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types'
 import classNames from 'classnames'
 import AtComponent from '../../common/component'
+import { AtFabProps } from 'types/fab'
 
-export default class AtFab extends AtComponent {
-  onClick () {
-    this.props.onClick && this.props.onClick(...arguments)
+export default class AtFab extends AtComponent<AtFabProps> {
+  public static defaultProps: AtFabProps
+  public static propTypes: InferProps<AtFabProps>
+
+  private onClick (): void {
+    this.props.onClick && this.props.onClick(arguments as any)
   }
 
-  render () {
+  public render (): JSX.Element {
     const { size, className } = this.props
 
     const rootClass = classNames('at-fab', className, {
--- types/fab.d.ts
+++ types/fab.d.ts
@@ -5,8 +5,14 @@ import { CommonEventFunction } from '@tarojs/components/types/common'
 import AtComponent from './base'
 
 export interface AtFabProps extends AtComponent {
+  /**
+   * 大小尺寸
+   * @default 'normal'
+   */
   size?: 'normal' | 'small'
-
+  /**
+   * 点击标签时触发
+   */
   onClick?: CommonEventFunction
 }
 
