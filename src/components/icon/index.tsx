5093389b697601e8351aa27543f6919b89eba4a1
Garfield Lee <openpgp@550.moe>
2019-11-04 16:59:38 +0800

    refactor(icon): 使用 TypeScript 重构 AtIcon

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,78 @@
+import Taro from '@tarojs/taro'
+import PropTypes, { InferProps } from 'prop-types'
+import classNames from 'classnames'
+import { Text } from '@tarojs/components'
+
+import AtComponent from '../../common/component'
+import { initTestEnv } from '../../common/utils'
+import { AtIconProps } from 'types/icon'
+
+initTestEnv()
+
+export default class AtIcon extends AtComponent<AtIconProps> {
+  public static defaultProps: AtIconProps
+  public static propTypes: InferProps<AtIconProps>
+
+  private handleClick (): void {
+    this.props.onClick && this.props.onClick(arguments as any)
+  }
+
+  public render (): JSX.Element {
+    const {
+      customStyle,
+      className,
+      prefixClass,
+      value,
+      size,
+      color
+    } = this.props
+
+    const rootStyle = {
+      fontSize: `${Taro.pxTransform(parseInt(String(size)) * 2)}`,
+      color
+    }
+
+    const iconName = value ? `${prefixClass}-${value}` : ''
+    return (
+      <Text
+        className={classNames(
+          prefixClass,
+          iconName,
+          className
+        )}
+        style={this.mergeStyle(rootStyle, customStyle as object)}
+        onClick={this.handleClick.bind(this)}
+      >
+      </Text>
+    )
+  }
+}
+
+AtIcon.defaultProps = {
+  customStyle: '',
+  className: '',
+  prefixClass: 'at-icon',
+  value: '',
+  color: '',
+  size: 24,
+  onClick: () => { },
+}
+
+AtIcon.propTypes = {
+  customStyle: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.string
+  ]),
+  className: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.string
+  ]),
+  prefixClass: PropTypes.string,
+  value: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  onClick: PropTypes.func,
+}
