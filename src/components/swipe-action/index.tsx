d68541a5c53079b3b1ea44a069eea72e3a1e42c6
Garfield Lee <openpgp@550.moe>
2020-04-11 19:28:17 +0800

    fix: 用了箭头函数不应再用 bind

--- src/components/swipe-action/index.tsx
+++ src/components/swipe-action/index.tsx
@@ -30,7 +30,7 @@ export default class AtSwipeAction extends AtComponent<
   private startX: number
   private startY: number
   private maxOffsetSize: number
-  private domInfo: Taro.rectElement
+  private domInfo: any
   private isMoving: boolean
   private isTouching: boolean
 
@@ -243,7 +243,7 @@ export default class AtSwipeAction extends AtComponent<
               <View
                 key={`${item.text}-${key}`}
                 style={item.style}
-                onClick={this.handleClick.bind(this, item, key)}
+                onClick={e => this.handleClick(item, key, e)}
                 className={classNames(
                   'at-swipe-action__option',
                   item.className
