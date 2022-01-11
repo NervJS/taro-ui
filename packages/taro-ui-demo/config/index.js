da821f273aa7daa3cd516d8496bae7381c90377c
shinken008 <shinken008@gmail.com>
2022-01-11 14:19:06 +0800

    chore: 锁定 colors 版本

--- packages/taro-ui-demo/config/index.js
+++ packages/taro-ui-demo/config/index.js
@@ -5,7 +5,7 @@ const config = {
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2,
+    828: 1.81 / 2
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
@@ -13,29 +13,29 @@ const config = {
   defineConstants: {},
   copy: {
     patterns: [],
-    options: {},
+    options: {}
   },
   framework: 'react',
   mini: {
     postcss: {
       pxtransform: {
         enable: true,
-        config: {},
+        config: {}
       },
       url: {
         enable: true,
         config: {
-          limit: 1024, // 设定转换尺寸上限
-        },
+          limit: 1024 // 设定转换尺寸上限
+        }
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]',
-        },
-      },
-    },
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
   },
   h5: {
     publicPath: '/',
@@ -43,17 +43,17 @@ const config = {
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {},
+        config: {}
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]',
-        },
-      },
-    },
-  },
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  }
 }
 
 module.exports = function (merge) {
