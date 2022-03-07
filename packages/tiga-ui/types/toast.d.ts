016085e07235bf71f0e9abcf7e99bdcef5b1122a
anjuke <anjuke@anjukedeMacBook-Pro.local>
2022-03-07 14:38:13 +0800

    feat: toast组件

--- packages/tiga-ui/types/toast.d.ts
+++ packages/tiga-ui/types/toast.d.ts
@@ -14,26 +14,16 @@ export interface AtToastProps extends AtComponent {
    */
   text?: string
   /**
-   * icon 的类型
-   */
-  icon?: string
-  /**
-   * 元素展示的图片
-   */
-  image?: string
-  /**
-   * 元素的状态
-   */
-  status?: 'error' | 'loading' | 'success'
-  /**
    * 元素持续的事件（设置为 0 将不会自动消失）
    * @default auto
    */
   duration?: string
+  // duration?: number
   /**
    * 是否存在底部遮罩层(无法点击底部的内容区)
    */
   maskHide?: boolean
+
   /**
    * 元素被点击之后触发的事件
    */
@@ -48,6 +38,22 @@ export interface AtToastState {
   _isOpened: boolean
 }
 
-declare const AtToast: ComponentClass<AtToastProps>
+export type ToastRef = {
+  // eslint-disable-next-line no-unused-vars
+  show: (params?: AtToastProps) => void
+  hide: () => void
+}
+
+export type ToastRefObj = {
+  current: ToastRef | null
+}
+
+interface ToastComponent extends ComponentClass<AtToastProps> {
+  // eslint-disable-next-line no-unused-vars
+  show: (params?: AtToastProps) => void
+  hide: () => void
+}
+
+declare const AtToast: ToastComponent
 
 export default AtToast
