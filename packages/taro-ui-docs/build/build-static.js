30cc03f8edd18998df38339eeca0c828296744e1
honlyHuang <huangliyn@gmail.com>
2023-11-09 23:12:21 +0800

    chore(docs): action deploy tcb (#1695)
    Co-authored-by: drl990114 <drl990114@gmail.com>

--- packages/taro-ui-docs/build/build-static.js
+++ packages/taro-ui-docs/build/build-static.js
@@ -9,12 +9,38 @@ spinner.start()
 // packages/taro-ui-docs/dist/h5
 fs.emptyDirSync(path.resolve(__dirname, '../dist/h5'))
 
-fs.copy(
-  // packages/taro-ui-demo/dist
-  path.resolve(__dirname, '../../taro-ui-demo/dist'),
-  path.resolve(__dirname, '../dist/h5')
-)
-  .then(() => {
+function mergeDemoH5AndDist() {
+  const demoH5Path = path.resolve(__dirname, '../../taro-ui-demo/dist')
+  if (!fs.existsSync(demoH5Path)) {
+    console.log('Please run `npm run build:demo` first')
     spinner.stop()
+    return
+  }
+
+  const distH5Path = path.resolve(__dirname, '../dist/h5')
+  const distPath = path.resolve(__dirname, '../dist')
+  const files = fs.readdirSync(demoH5Path)
+
+  const promises = []
+  files.forEach(file => {
+    if (file !== 'index.html') {
+      promises.push(
+        fs.copy(path.resolve(demoH5Path, file), path.resolve(distPath, file))
+      )
+    } else {
+      promises.push(
+        fs.copy(path.resolve(demoH5Path, file), path.resolve(distH5Path, file))
+      )
+    }
   })
-  .catch(err => console.error(err))
+
+  Promise.all(promises)
+    .then(() => {
+      spinner.stop()
+    })
+    .catch(err => {
+      console.log('build static error:', err)
+    })
+}
+
+mergeDemoH5AndDist()
